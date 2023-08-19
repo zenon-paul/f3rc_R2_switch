2055df1aff1d32884e546891d36449b5c0e4bf7c
zenon-paul <sakura_1024@ja2.so-net.ne.jp>
2023-08-19 12:56:37 +0900

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,22 @@ using ThisThread::sleep_for;
 
 
 InterruptIn sw(D6);
+DigitalOut led(D10);
 
-int swstats = 0;
+int swstatus = 0;
 int flag = 0;
+
 unsigned int checker = 0;
 unsigned int unsintmax = ~0;
 
 void fallfunc(){//fall
-    if(swstats){
-        swstats = 0;
+    if(swstatus){
+        swstatus = 0;
     }
 }
 void risefunc(){//rise
-    if(swstats == 0){
-        swstats = 1;
+    if(swstatus == 0){
+        swstatus = 1;
     }
 }
 
@@ -26,16 +28,12 @@ int main(){
     sw.rise(risefunc);
     sw.fall(fallfunc);
     while(1){
-        checker = checker << 1;
-        checker += (swstats == 1)?1:0;
-        if(checker == unsintmax){
-            flag = 1;
-        }
-        else{
-            flag = 0;
-        }
-        printf("swstatus:%d flag:%d\n",swstats,flag);
+        checker = (checker << 1) + swstatus;
+        flag = (checker == unsintmax);
+        led = flag;
+        printf("sw:%d flag:%d\n",swstatus,flag);
     }
 }
+
 
 
